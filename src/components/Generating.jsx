c99f0644c2f17923d0f835b82a943ebf1b02d615
Nicolas <nicolas.chapron@uleis.co.uk>
2024-05-18 22:15:10 +0100

    Added loading animation

--- src/components/Generating.jsx
+++ src/components/Generating.jsx
@@ -7,7 +7,7 @@ const Generating = ({ className }) => {
         className || ""
       } text-base`}
     >
-      <img src={loading} alt="loading" className="w-5 h-5 mr-4" />
+      <img src={loading} alt="loading" className="w-5 h-5 mr-4 animate-spin" />
       AI is generating
     </div>
   );
